48b44a13456dd2478f7b0bb865f815704aa388b2
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-11-19 11:59:15 +0100

    homepage

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Chi siamo</h1>
+      <p>Questa è la pagina che racconta la nostra storia e cosa facciamo.</p>
+    </div>
+  );
+};
+
+export default About;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,22 @@
+import { Col, Container, Row } from "react-bootstrap";
 const Home = () => {
-  return <h1>ciao stronzi</h1>;
+  return (
+    <Container class="text-center">
+      <Row>
+        <Col>
+          <h1>Mjorn</h1>
+        </Col>
+        <Col>
+          <img
+            src="C:\Users\franc\OneDrive\Desktop\proggetto finale\MjornStoryTelling\mjorn-story-telling\src\assets\img\logo.png"
+            alt="Logo MjornStoryTelling"
+          />
+        </Col>
+        <Col>
+          <h1>StoryTelling</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 export default Home;
